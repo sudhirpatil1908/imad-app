fc172017d8c5a731a93f9c98b4bdb9d200521601
sudhirpatil1908 <31499245+sudhirpatil1908@users.noreply.github.com>
2017-09-01 11:50:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-first', function (req, res) {
+  res.sendFile('Hi am your first javascript page');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
